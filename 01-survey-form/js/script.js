cfbab19a10b0da213b8a03a71cd580c5a638cc15
Médéric <mederic.math.pv@gmail.com>
2025-02-07 14:31:15 +0100

    Updating files

--- 01-survey-form/js/script.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById("survey-form");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log("Le formulaire a été soumis.");
-});
